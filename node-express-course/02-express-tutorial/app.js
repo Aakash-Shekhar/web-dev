4dd5dcb0cee4c65c6286d2141f9f41e8b859a21a
Aakash <aakashshekhar25@gmail.com>
2024-02-28 17:36:04 +0530

    middleware revision

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,41 +1,12 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+let { people } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Home Page</h1><a href="/api/products">Products</a>`);
-});
+// static assets
+app.use(express.static("./methods-public"));
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-
-  res.json(newProducts);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-  console.log(req);
-  console.log(req.params);
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product not found");
-  }
-  return res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("Hello World");
-});
-
-app.get("/api/v1/query", (req, res) => {
-  console.log(req.query);
-  res.send("Hello World");
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
 app.listen(5000, () => {
